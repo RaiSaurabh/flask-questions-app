27a10620b3e9a1b4034278fe75ea2a577b19e8a7
Saurabh Rai <rai.saurabh@hotmail.com>
2024-08-05 16:06:39 +0530

    Update flaskapp.py

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -14,6 +14,15 @@ application.secret_key = os.environ['FLASK_SECRET']
 
 openai.api_key  = "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"
 
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
 @application.route('/healthz')
 def healthz():
     """
